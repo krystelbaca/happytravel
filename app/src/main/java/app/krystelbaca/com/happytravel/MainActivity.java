4c5fe4fa2d0d97bd2650d34b418ed0d0df287d4b
Krystel Baca Salayandia <salayandia.k@gmail.com>
2017-03-04 03:53:23 -0600

    added IDs for buttons

--- app/src/main/java/app/krystelbaca/com/happytravel/MainActivity.java
+++ app/src/main/java/app/krystelbaca/com/happytravel/MainActivity.java
@@ -1,13 +1,30 @@
 package app.krystelbaca.com.happytravel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnDestinos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnDestinos = (Button) findViewById(R.id.btn_destinos);
+
+        btnDestinos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent destinosIntent = new Intent(getBaseContext(), DestinoListActivity.class);
+                startActivity(destinosIntent);
+            }
+        });
     }
+
+
 }
