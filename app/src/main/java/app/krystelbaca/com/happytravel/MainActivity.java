cac3fcfd5a17d4d9367f8cfebd12865a9708c9d2
Krystel Baca Salayandia <salayandia.k@gmail.com>
2017-03-04 04:06:37 -0600

    Implemented viaje list

--- app/src/main/java/app/krystelbaca/com/happytravel/MainActivity.java
+++ app/src/main/java/app/krystelbaca/com/happytravel/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnDestinos;
+    private Button btnViajes;
+    private Button btnOfertas;
+    private Button btnCuenta;
 
 
     @Override
@@ -16,12 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnDestinos = (Button) findViewById(R.id.btn_destinos);
+        btnViajes = (Button) findViewById(R.id.btn_viajes);
+        btnOfertas = (Button) findViewById(R.id.btn_ofertas);
+        btnCuenta = (Button) findViewById(R.id.btn_cuenta);
 
         btnDestinos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent destinosIntent = new Intent(getBaseContext(), DestinoListActivity.class);
                 startActivity(destinosIntent);
+            }
+        });
+
+        btnViajes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viajesIntent = new Intent(getBaseContext(),MiViajeListActivity.class);
+                startActivity(viajesIntent);
+
             }
         });
     }
